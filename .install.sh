d6186610abd10158b67b59335b495d8b738ad77e
Khue Doan <khuedoan98@gmail.com>
2019-11-28 14:35:42 +0700

    Remove dash to dock

--- .install.sh
+++ .install.sh
@@ -80,7 +80,7 @@ install_core_packages() {
 install_extra_packages() {
     sudo pacman --noconfirm --needed -S arc-gtk-theme aria2 man noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra tmux tree unrar youtube-dl xorg-xprop
     gpg --recv-keys EB4F9E5A60D32232BB52150C12C87A28FEAC6B20
-    trizen --noconfirm --needed -S --sudo-autorepeat-at-runtime chromium-vaapi-bin gnome-shell-extension-dash-to-dock gnome-shell-extension-unite la-capitaine-icon-theme-git nerd-fonts-source-code-pro ttf-ms-fonts
+    trizen --noconfirm --needed -S --sudo-autorepeat-at-runtime chromium-vaapi-bin gnome-shell-extension-unite la-capitaine-icon-theme-git nerd-fonts-source-code-pro ttf-ms-fonts
 }
 
 install_onedark_terminal_theme() {
